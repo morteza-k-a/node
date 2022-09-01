5756f963fbb99002de5182e6e38e15262e69aaf6
morteza-k-a <68350615+morteza-k-a@users.noreply.github.com>
2022-09-02 01:53:52 +0430

    'next2'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraph = require('telegraf');
 
-const bot = new Telegraph('304694590:AAFT_FllrfjRjNx9M6Ss8vG6e-LUNjU_sFc');
+const bot = new Telegraph('5743218335:AAGXZxq2xif4a53bOFz7YoB6mnfWyu59DyA');
 bot.command('start', ctx => {
     console.log(ctx.from)
     bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
